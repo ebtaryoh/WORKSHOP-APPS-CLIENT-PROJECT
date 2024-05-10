b20bf69ab3a1ae5ac7b7924c6ba68c25f012e9c7
Ibitayo Akinnibosun <ibitayo.akinnibosun@gmail.com>
2024-05-10 13:07:15 +0100

    new changes

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -3,13 +3,14 @@ import { Dropdown, Image, Navbar } from "react-bootstrap";
 import DropDown from "./DropDown";
 import logo3 from "../../src/images/logo3.png" 
 import logo1 from "../../src/images/logo1.png"
+import { Link } from "react-router-dom";
 
 const Navbar1 = () => {
   return (
     <Navbar className="shadow">
       <div className="d-flex flex-row gap-3 align-items-center m-auto my-1 image-container navbar-res ">
-        <Image className="mb-4 top-image" src={logo1} />
-        <Image className="mb-4  " src={logo3} />
+        <Link to="/"><Image className="mb-4 top-image" src={logo1} />
+        <Image className="mb-4  " src={logo3} /></Link>
         <DropDown />
       </div>
       <hr />
